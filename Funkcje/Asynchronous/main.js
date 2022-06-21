449719d86d50d1259c1b12ccbfbb9f3fa9bead33
LonlehKitteh <krzysztof02gach@gmail.com>
2022-06-21 18:31:38 +0200

    Async functions

--- Funkcje/Asynchronous/main.js
+++ Funkcje/Asynchronous/main.js
@@ -1,1 +1,39 @@
-// testing gpg key
+const getUserData = () => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log('1. get user data')
+            resolve({ name: 'Jon' })
+        }, 600)
+    })
+}
+
+const registerUser = (user) => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log('2. register user')
+            resolve({ name: user.name, id: 42 })
+        }, 400)
+    })
+}
+
+const sendEmail = (user) => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log(`Name: ${user.name}, id: ${user.id}`)
+            resolve()
+        }, 500)
+    })
+}
+// getUserData()
+// .then(registerUser)
+// .then(sendEmail)
+// .then(() => console.log("end!"))
+
+async function someAsyncFunction() {
+    const userData = await getUserData()
+    const registeredUser = await registerUser(userData)
+    await sendEmail(registeredUser)
+    console.log('end!')
+}
+
+someAsyncFunction()

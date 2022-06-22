44c8c64a7d9a3f4d2a767eb445f27c62be458e39
LonlehKitteh <krzysztof02gach@gmail.com>
2022-06-22 23:28:03 +0200

    calculator part 3

--- Calculator 2022/main.js
+++ Calculator 2022/main.js
@@ -1,7 +1,7 @@
 const buttons = document.querySelectorAll('button')
 const screenValue = document.querySelector('.value')
 const history = document.querySelector(".history")
-var fanish = false
+var vanish = false
 
 const buttonHandle = () => {
     buttons.forEach((button, key) => {
@@ -11,33 +11,38 @@ const buttonHandle = () => {
 
 const callback = (e, key) => {
     let value = screenValue.value
-    if (fanish) {
-        fanish = false
-        return e.target.innerHTML
-    }
 
-    // main part
+    if ([8, 9, 10, 12, 13, 14, 16, 17, 18, 21].includes(key)) {
+        if (vanish) {
+            vanish = false
+            return (history.innerHTML + value).split(' ')[(history.innerHTML + value).split(' ').length - 1] = e.target.innerHTML
+        }
+        return (value + e.target.innerHTML).split(' ').map(e => +e === Number(e) ? +e : e).join(' ')
+    }
+    if (vanish) return value
+
     if (key === 0) return factorialize(value)
     if (key === 1) return [...value.split(' ').slice(0, -1), 0].join(' ')
-    if (key === 2) return 0
+    if (key === 2) {
+        history.innerHTML = ''
+        return 0
+    }
     if (key === 3) return value.length !== 1 ? value.substring(0, value.length - 1) : 0 // repair
     if (key === 4) return +value === 0 ? 0 : 1 / value
-    if (key === 5) {
-        return history.innerHTML += Math.sqrt(value.split(' ')[value.split(' ').length - 1]).toFixed(8)
-    }
+    if (key === 5) return value.split(' ').length !== 1 ? history.innerHTML += Math.sqrt(value.split(' ')[value.split(' ').length - 1]).toFixed(8) : Math.sqrt(value.split(' ')[value.split(' ').length - 1]).toFixed(8)
     if (key === 6) return Math.pow(value, 2)
     if ([7, 11, 15, 19].includes(key)) {
-        console.log(e.target.value)
-        history.innerHTML = value + ` ${e.target.value} `
-        fanish = true
+        history.innerHTML = value + ` ${key === 7 ? '/' : key === 11 ? '*' : key === 15 ? '-' : '+'} `
+        vanish = true
         return value
     }
     if (key === 20) return value * -1
     if (key === 22) return Number(value + '.' + 1) || value.split(' ')[value.split(' ').length - 1].length === 1 ? value + '.' : value
-    if (key === 23) return eval(history.innerHTML + value)
-    if ([8, 9, 10, 12, 13, 14, 16, 17, 18, 21].includes(key))
-        return (value + e.target.innerHTML).split(' ').map(e => +e === Number(e) ? +e : e).join(' ')
-
+    if (key === 23) {
+        history.innerHTML += value
+        if (history.innerHTML.endsWith('/ 0')) return 0
+        return eval(history.innerHTML)
+    }
 }
 
 buttonHandle()

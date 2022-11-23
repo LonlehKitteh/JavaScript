513ec4f610a4cccd1f910647bd33c63073f42103
Krzysztof Gach <krzysztof02gach@gmail.com>
2022-11-23 20:30:19 +0100

    equalizer 3

--- Canvas/Equalizer/main.js
+++ Canvas/Equalizer/main.js
@@ -1,64 +1,44 @@
-import { audio } from './sound.js'
+const btn = document.querySelector('.btn');
+const audio = document.querySelector('audio');
+const visualizer = document.querySelector('.visualizer');
 
-const btn1 = document.getElementById("btn1")
-const audioCtx = new AudioContext()
-const audioCtx1 = new AudioContext()
+btn.addEventListener('click', e => {
+    audio.paused ? audio.play() : audio.pause();
+    btn.classList.toggle('btn-play');
+    btn.classList.toggle('btn-pause');
+});
 
-btn1.addEventListener("click", () => {
-    audio.play()
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+const ctx = new window.AudioContext();
+const analyser = ctx.createAnalyser();
+const source = ctx.createMediaElementSource(audio);
+source.connect(analyser);
+source.connect(ctx.destination);
+analyser.fftSize = 64;
+const bufferLength = analyser.frequencyBinCount;
 
-    audio.addEventListener("playing", () => {
-        console.log("hi")
-    })
+let dataArray = new Uint8Array(bufferLength);
+let elements = [];
+for (let i = 0; i < bufferLength; i++) {
+    const element = document.createElement('span');
+    element.classList.add('element');
+    elements.push(element);
+    visualizer.appendChild(element);
+}
 
-    audio.addEventListener("ended", () => {
-        console.log("bye")
-    })
-})
+const clamp = (num, min, max) => {
+    if (num >= max) return max;
+    if (num <= min) return min;
+    return num;
+}
 
-const btn2 = document.getElementById("btn2")
-btn2.addEventListener("click", () => {
-    const oscillator = audioCtx1.createOscillator();
-    oscillator.connect(audioCtx1.destination);
-    oscillator.type = "triangle"
-    oscillator.start()
-    setTimeout(() => oscillator.stop(), 100)
-})
-
-const container = document.getElementById('container')
-const canvas = document.getElementById('canvas')
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
-const ctx = canvas.getContext('2d')
-let audioSource, analyser
-
-container.addEventListener('click', () => {
-    audio.play()
-    audioSource = audioCtx.createMediaElementSource(audio)
-    analyser = audioCtx.createAnalyser()
-    audioSource.connect(analyser)
-    analyser.connect(audioCtx.destination)
-    analyser.fftSize = 64 // higher number more canvas
-    const bufferLength = analyser.frequencyBinCount
-    const dataArray = new Uint8Array(bufferLength)
-
-    const barWidth = canvas.width / bufferLength
-    let barHeight
-    let x = 0
-
-    function animate() {
-        x = 0
-        ctx.clearRect(0, 0, canvas.width, canvas.height)
-        analyser.getByteFrequencyData(dataArray)
-
-        for (let i = 0; i < bufferLength; i++) {
-            barHeight = dataArray[i]
-            ctx.fillStyle = "white"
-            ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight)
-            x += barWidth
-        }
-
-        requestAnimationFrame(animate)
+const update = () => {
+    requestAnimationFrame(update);
+    analyser.getByteFrequencyData(dataArray);
+    for (let i = 0; i < bufferLength; i++) {
+        let item = dataArray[i];
+        item = item > 150 ? item / 1.5 : item * 1.5;
+        elements[i].style.transform = `rotateZ(${i * (360 / bufferLength)}deg) translate(-50%, ${clamp(item, 100, 150)}px)`;
     }
-    animate()
-})
+};
+update();

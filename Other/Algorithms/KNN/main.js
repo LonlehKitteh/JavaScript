41dbf4df0c37ec0beb7c81e693405cfbd73c10c3
LonlehKitteh <krzysztof02gach@gmail.com>
2022-11-28 19:04:53 +0100

    knn 3

--- Other/Algorithms/KNN/main.js
+++ Other/Algorithms/KNN/main.js
@@ -4,7 +4,6 @@ function setPoints(n) {
     Math.floor(Math.random() * 10)
     for (let i = 0; i < n; i++) {
         let temp = {
-            index: i,
             x: +(Math.random() * 10 - 5).toFixed(2),
             y: +(Math.random() * 10 - 5).toFixed(2),
             className: `Class ${classes[Math.floor(Math.random() * 3)]}`
@@ -19,29 +18,32 @@ function setPoints(n) {
 function KNN(k) {
     const trainedData = setPoints(100)
     const testData = setPoints(10).map(obj => ({ ...obj, className: "" }))
-    let temp = [...testData, ...trainedData].sort((a, b) => a.hypot - b.hypot)
+    let temp = [...testData, ...trainedData].sort((a, b) => a.hypot - b.hypot).map((e, i) => ({ ...e, index: i }))
 
+    temp.forEach((obj, i) => {
+        if (!obj.className) {
+            let n = k / 2
+            while (n >= -k / 2) {
+                // if (!temp[i + n].className) continue
+                console.log(temp[i + n])
 
-    let indexes = []
-
-    temp.forEach((obj, index) => {
-        if (!obj.className) indexes.push({ index: index, resolved: [] })
-    })
-
-    indexes.map(obj => {
-        let resolved = []
-
-        for (let i = 0; i < 2; i++) {
-
-            // if (obj.index === temp.length - 1) return ({ resolved: temp.slice(-k + i) })
-            // if (obj.index === 0) return ({ resolved: temp.slice(i, k) })
-
-            console.log(temp[obj.index + 1])
+                n--;
+            }
         }
-
-        return ({ ...obj, resolved: resolved })
     })
-    console.log(indexes)
+
+    // indexes.map(obj => {
+    // console.log(obj)
+    // console.log(temp.slice(obj.index - (k / 2), obj.index + (k / 2) + 1), obj.index)
+
+    // if (obj.index === temp.length - 1) return ({ resolved: temp.slice(-k + i) })
+    // if (obj.index === 0) return ({ resolved: temp.slice(i, k) })
+
+    // console.log(temp.slice(k - 2 + obj.index, k + 2 + obj.index))
+
+    // return ({ ...obj, resolved: resolved })
+    // })
+    // console.log(indexes)
 
 }
 
